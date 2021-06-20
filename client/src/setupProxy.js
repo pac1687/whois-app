4d9fe5d27c48681c455c8df3230319d0389b5ea6
Justin Pacubas <jpacubas@gmail.com>
2021-06-20 16:09:46 -0500

    Update proxy address and port

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/lookup',
+    '/graphql',
     createProxyMiddleware({
-      target: `http://backend:8000`,
+      target: `http://backend:4000`,
       changeOrigin: true,
     })
   );
