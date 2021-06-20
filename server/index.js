370920eb18c9bc04ca6a51e324e52c562f044cd3
Justin Pacubas <jpacubas@gmail.com>
2021-06-19 21:12:46 -0500

    Use cors and unwrap data from whois

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const axios = require('axios');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+app.use(cors())
 
 app.get("/lookup", async (req, res) => {
   const { domain } = req.query;
   try {
     const whoisResponse = await axios.get(`https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=${process.env.WHOIS_API_KEY}&domainName=${domain}&outputFormat=JSON`);
     console.log(whoisResponse.data);
-    res.json({ data: whoisResponse.data });
+    res.json(whoisResponse.data);
   } catch (error) {
     console.error(error);
   }
